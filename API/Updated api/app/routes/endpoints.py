293ce141b868395f2b73cb983dc7a0d4f8d3299b
Atlihero <atlihero@gmail.com>
2026-03-30 14:11:56 +0000

    Update endpoints.py

--- API/Updated api/app/routes/endpoints.py
+++ API/Updated api/app/routes/endpoints.py
@@ -1,4 +1,26 @@
 # Task C5
+from datetime import datetime
+
+from fastapi import APIRouter, Depends, File, Form, UploadFile
+from sqlalchemy.orm import Session
+
+from app.db.session import get_orkuflaedi_session
+from app.models.monthly_company_usage_model import MonthlyCompanyUsageModel
+from app.models.monthly_energy_flow_model import MonthlyPlantEnergyFlowModel
+from app.models.monthly_plant_loss_ratios import MonthlyPlantLossRatiosModel
+from app.services.service import (
+    OrkuEiningar,
+    Maelingar,
+    Notandi,
+    Stod,
+    TegundMaelingar,
+    PowerPlantInjection,
+    CustomerWithdrawal,
+)
+from app.utils.validate_date_range import validate_date_range_helper
+
+router = APIRouter()
+db_name = "OrkuFlaediIsland"
 
 '''
 Endpoint 1: get_monthly_energy_flow()

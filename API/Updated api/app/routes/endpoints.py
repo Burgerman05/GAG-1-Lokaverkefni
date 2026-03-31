6974420d1b418c7ddf926999d47b9cf52c3388ac
JoelA <joelarnar@gmail.com>
2026-03-31 13:15:00 +0000

    Formatting

--- API/Updated api/app/routes/endpoints.py
+++ API/Updated api/app/routes/endpoints.py
@@ -1,24 +1,27 @@
 # Task C5
+from fastapi import APIRouter
 
-'''
+router = APIRouter()
+
+"""
 Endpoint 1: get_monthly_energy_flow()
-'''
+"""
 
-'''
+"""
 Endpoint 2: get_monthly_company_usage()
-'''
+"""
 
-'''
+"""
 Endpoint 3: get_monthly_plant_loss_ratios()
-'''
+"""
 
 # Task E1
 
-'''
+"""
 Endpoint 4: insert_measurements()
-'''
+"""
 
 # Task F1
-'''
+"""
 Endpoint 5: get_substations_gridflow()
-'''
+"""
